de425efcc51dcc9e6bee8ee1d094b0d89cdb04cf
jafary <jafaryesmaeil340@gmail.com>
2020-01-31 08:54:17 +0330

    جمعه علیزاده ای

--- funcs.php
+++ funcs.php
@@ -1,7 +1,7 @@
 <?php
-$connect = mysqli_connect("localhost","root","","jafary");
-		MySQLi_query($connect,"SET CHARACTER SET utf8");
-/*include "sqli.php";
+/*$connect = mysqli_connect("localhost","root","","jafary");
+		MySQLi_query($connect,"SET CHARACTER SET utf8");*/
+include "sqli.php";
 	
 		$connect = mysqli_connect($adddb,$dbuser,$dbpass,$dbname);
 		MySQLi_query($connect,"SET CHARACTER SET utf8");
